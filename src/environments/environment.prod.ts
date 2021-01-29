f0c0516778b23580186f9ba0c3c4e7cc6fb1c312
shubhamknoldus <shubham.verma@knoldus.com>
2021-01-29 08:58:53 +0530

    chnage host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serviceUrl: 'http://localhost'
+  serviceUrl: 'http://192.168.0.108'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  serviceUrl : 'http://localhost'
+  serviceUrl : 'http://192.168.0.108'
 };
 
 /*
